ac3d32e53b3fa94ea9116f25588c5384c9d5f5a3
CY2012 <1310891926@qq.com>
2015-08-06 17:36:22 +0800

    Changes to be committed:

---
+++ jeasyuidemo/src/main/java/com/wyhello/jeasyuidemo/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.wyhello.jeasyuidemo.service.impl;
+
+import java.util.List;
+
+import com.wyhello.jeasyuidemo.dao.UserDAO;
+import com.wyhello.jeasyuidemo.dao.impl.UserDAOImpl;
+import com.wyhello.jeasyuidemo.pojo.User;
+import com.wyhello.jeasyuidemo.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	private UserDAO userDao = new UserDAOImpl();
+	
+	public List<User> queryAllUser() {
+		List<User> l = userDao.queryAll();
+		return l;
+	}
+
+	public void addUser(User u) {
+		userDao.add(u);
+		
+	}
+
+	public void updateUser(User u) {
+		userDao.update(u);
+		
+	}
+
+	public void removeUser(User u) {
+		userDao.remove(u);
+	}
+
+}
